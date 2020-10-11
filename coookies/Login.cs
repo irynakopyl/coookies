50dc1183856b43451ca81ff50a8c8677e0bcee98
iryna.kopyl <iruna.kopyl@gmail.com>
2020-10-11 13:33:43 +0300

    Add project files.

---
+++ coookies/Login.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace coookies
+{
+    class Login
+    {
+        private readonly string _appUrl;
+        private CookieContainer _authCookie;
+        private readonly string _authServiceUrl;
+        private readonly string _recordServiceUrl;
+        private readonly string _userName;
+        private readonly string _userPassword;
+        public Login(string appUrl, string userName, string userPassword)
+        {
+            _appUrl = appUrl;
+            _authServiceUrl = _appUrl + @"/ServiceModel/AuthService.svc/Login";
+            _recordServiceUrl = _appUrl + @"/0/rest/UsrTestService/AddRecord";
+            _userName = userName;
+            _userPassword = userPassword;
+        }
+        public void TryLogin()
+        {
+            var authData = @"{
+                    ""UserName"":""" + _userName + @""",
+                    ""UserPassword"":""" + _userPassword + @"""
+                }";
+            var request = CreateRequest(_authServiceUrl, authData);
+            _authCookie = new CookieContainer();
+            request.CookieContainer = _authCookie;
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        var responseMessage = reader.ReadToEnd();
+                        Console.WriteLine(responseMessage);
+                        if (responseMessage.Contains("\"Code\":1"))
+                        {
+                            throw new UnauthorizedAccessException($"Unauthorized {_userName} for {_appUrl}");
+                        }
+                    }
+                    string authName = ".ASPXAUTH";
+                    string authCookeValue = response.Cookies[authName].Value;
+                    _authCookie.Add(new Uri(_appUrl), new Cookie(authName, authCookeValue));
+                }
+            }
+        }
+
+        public void AddRecord(string name)
+        {
+            var data = @"{
+                    ""name"":""" + name + @"""
+                }";
+            var request = CreateRequest(_recordServiceUrl, data);
+            request.CookieContainer = _authCookie;
+            AddCsrfToken(request);
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        var responseMessage = reader.ReadToEnd();
+                        Console.WriteLine(responseMessage);
+                        if (responseMessage.Contains("\"Code\":1"))
+                        {
+                            throw new UnauthorizedAccessException($"Unauthorized {_userName} for {_appUrl}");
+                        }
+                    }
+                    string authName = ".ASPXAUTH";
+                    string authCookeValue = response.Cookies[authName].Value;
+                    _authCookie.Add(new Uri(_appUrl), new Cookie(authName, authCookeValue));
+                }
+            }
+        }
+
+        private void AddCsrfToken(HttpWebRequest request)
+        {
+            var cookie = request.CookieContainer.GetCookies(new Uri(_appUrl))["BPMCSRF"];
+            if (cookie != null)
+            {
+                request.Headers.Add("BPMCSRF", cookie.Value);
+            }
+        }
+        private HttpWebRequest CreateRequest(string url, string requestData = null)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            request.KeepAlive = true;
+            if (!string.IsNullOrEmpty(requestData))
+            {
+                using (var requestStream = request.GetRequestStream())
+                {
+                    using (var writer = new StreamWriter(requestStream))
+                    {
+                        writer.Write(requestData);
+                    }
+                }
+            }
+            return request;
+        }
+    }
+}
---
+++ coookies/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace coookies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Login login = new Login("https://bpmtest.ukr-china.eu", "testuser", "testuser333");
+            Console.WriteLine("First request`s response:");
+            login.TryLogin();
+            Console.WriteLine("Second request`s response:");
+            login.AddRecord("Iryna");
+            Console.ReadLine();
+        }
+
+    }
+   
+}
